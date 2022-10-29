e8d2935018bfcf204246804049efd9283d6142b2
Ricardo Duarte <rykardo.contato@gmail.com>
2022-10-28 21:22:53 -0300

    first commit

--- arquivo/operacoes_matematicas/multiplicacao_e_divisao.py
+++
@@ -1,6 +0,0 @@
-def multiplica_dois_numeros(a,b):
-  return a * b
-
-
-def divide_dois_numeros(a,b):
-  return a/b
--- arquivo/operacoes_matematicas/soma_e_subtrai.py
+++
@@ -1,7 +0,0 @@
-def soma_dois_numeros(a, b):
-    return a + b
-
-
-
-def subtrai_dois_numeros(a,b):
-  return a - b
